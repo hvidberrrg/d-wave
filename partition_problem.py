f32377d0b22e460c9ea816685de52e33e86d4b7f
proxy-s.hvidberg@gmail.com <s.hvidberg@gmail.com>
2021-04-17 18:43:00 +0000

    Refactor sample code

--- partition_problem.py
+++ partition_problem.py
@@ -13,10 +13,13 @@ def generate_partition_problem_bqm(s):
     vartype = dimod.BINARY
     return dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
 
+def sample_dwave(s):
+    bqm = generate_partition_problem_bqm(s)
+    sampler = LeapHybridSampler()
+    sampleset = sampler.sample(bqm, label="Partition Problem (" + str(len(s)) + " numbers)")
+    return sampleset.first.sample
+
 #s = [4, 2, 7, 1]
 s = [25, 7, 13, 31, 42, 17, 21, 10]
-bqm = generate_partition_problem_bqm(s)
-sampler = LeapHybridSampler()
-sampleset = sampler.sample(bqm, label="Partition Problem")
-sample = sampleset.first.sample
+sample = sample_dwave(s)
 print(sample)
