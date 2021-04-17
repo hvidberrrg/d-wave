22bc69412bdbf02f786a76a139202344660182d6
proxy-s.hvidberg@gmail.com <s.hvidberg@gmail.com>
2021-04-17 18:53:11 +0000

    Partition numbers based on dwave sample

--- partition_problem.py
+++ partition_problem.py
@@ -9,6 +9,7 @@ def generate_partition_problem_bqm(s):
         linear[i] = s[i]*(s[i] - c)
         for j in range(1 + i, len(s)):
             quadratic[(i, j)] = 2*s[i]*s[j]
+    
     offset = 0.0
     vartype = dimod.BINARY
     return dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
@@ -19,7 +20,20 @@ def sample_dwave(s):
     sampleset = sampler.sample(bqm, label="Partition Problem (" + str(len(s)) + " numbers)")
     return sampleset.first.sample
 
+def partition_numbers(s, sample):
+    s1 = list()
+    s2 = list()
+    for i in range(len(sample)):
+        if sample[i] == 1:
+            s1.append(s[i]) 
+        else:
+            s2.append(s[i])
+    
+    return s1, s2
+
 #s = [4, 2, 7, 1]
 s = [25, 7, 13, 31, 42, 17, 21, 10]
 sample = sample_dwave(s)
-print(sample)
+s1, s2 = partition_numbers(s, sample)
+print(s1)
+print(s2)
