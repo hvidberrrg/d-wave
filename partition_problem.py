6fdcfccc4085f9ece88fc5c99792cc4140ba0f56
proxy-s.hvidberg@gmail.com <s.hvidberg@gmail.com>
2021-04-17 18:28:20 +0000

    Generate binary quadratic model from list of numbers

--- partition_problem.py
+++ partition_problem.py
@@ -1,15 +1,22 @@
 import dimod
 from dwave.system import LeapHybridSampler
 
-linear = {1: -40, 2: -24, 3: -49, 4: -13}
-quadratic = {(1, 2): 16, (1, 3): 56, (1, 4): 8,
-             (2, 3): 28, (2, 4): 4,
-             (3, 4): 14}
-offset = 0.0
-vartype = dimod.BINARY
-bqm = dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
+def generate_partition_problem_bqm(s):
+    linear = dict()
+    quadratic = dict()
+    c = sum(s)
+    for i in range(len(s)):
+        linear[i] = s[i]*(s[i] - c)
+        for j in range(1 + i, len(s)):
+            quadratic[(i, j)] = 2*s[i]*s[j]
+    offset = 0.0
+    vartype = dimod.BINARY
+    return dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
 
+#s = [4, 2, 7, 1]
+s = [25, 7, 13, 31, 42, 17, 21, 10]
+bqm = generate_partition_problem_bqm(s)
 sampler = LeapHybridSampler()
-sampleset = sampler.sample(bqm)
+sampleset = sampler.sample(bqm, label="Partition Problem")
 sample = sampleset.first.sample
 print(sample)
