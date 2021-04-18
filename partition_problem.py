9df5d6a103b995997bd0f14f075146c25070f5e1
hvidberrrg <s.hvidberg@gmail.com>
2021-04-18 11:47:48 +0000

    Ensure PEP 8 compliance and avoid warning regarding shadowing variable from outer scope

--- partition_problem.py
+++ partition_problem.py
@@ -1,6 +1,7 @@
 import dimod
 from dwave.system import LeapHybridSampler
 
+
 def generate_partition_problem_bqm(s):
     linear = dict()
     quadratic = dict()
@@ -14,17 +15,19 @@ def generate_partition_problem_bqm(s):
     vartype = dimod.BINARY
     return dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
 
-def sample_dwave(bqm, sampleLabel):
+
+def sample_dwave(bqm, sample_label):
     sampler = LeapHybridSampler()
-    sampleset = sampler.sample(bqm, label = sampleLabel)
-    return sampleset.first.sample
+    sample_set = sampler.sample(bqm, label = sample_label)
+    return sample_set.first.sample
+
 
 def partition_numbers(s):
     s1 = list()
     s2 = list()
     bqm = generate_partition_problem_bqm(s)
-    sampleLabel = "Partition Problem (" + str(len(s)) + " numbers)"
-    sample = sample_dwave(bqm, sampleLabel)
+    sample_label = "Partition Problem (" + str(len(s)) + " numbers)"
+    sample = sample_dwave(bqm, sample_label)
     for i in range(len(sample)):
         if sample[i] == 1:
             s1.append(s[i]) 
@@ -33,14 +36,20 @@ def partition_numbers(s):
     
     return s1, s2
 
+
 def perfectness(s1, s2):
     return abs(sum(s1) - sum(s2))
 
-#s = [4, 2, 7, 1]
-#s = [25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1, 25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1, 25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1, 25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1]
-s = [25, 7, 13, 31, 42, 17, 21, 10]
-s1, s2 = partition_numbers(s)
 
-print(s1)
-print(s2)
-print("Perfectness: " + str(perfectness(s1, s2)))
+def main():
+    #s = [4, 2, 7, 1]
+    #s = [25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1, 25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1, 25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1, 25, 7, 13, 31, 42, 17, 21, 10, 4, 3, 8, 1]
+    s = [25, 7, 13, 31, 42, 17, 21, 10]
+    s1, s2 = partition_numbers(s)
+
+    print(s1)
+    print(s2)
+    print("Perfectness: " + str(perfectness(s1, s2)))
+
+
+main()
