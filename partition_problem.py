fecc2c43873f9d2b89d7e940df5d2b3e59c8c0c8
hvidberrrg <s.hvidberg@gmail.com>
2021-04-21 19:49:47 +0000

    Move the sampler code to a separate module

--- partition_problem.py
+++ partition_problem.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import dimod
-from dwave.system import LeapHybridSampler
+import sampler
 
 
 def generate_partition_problem_bqm(set_of_numbers: list[int]) -> dimod.BinaryQuadraticModel:
@@ -31,24 +31,6 @@ def generate_partition_problem_bqm(set_of_numbers: list[int]) -> dimod.BinaryQua
     return dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
 
 
-def sample_dwave(bqm: dimod.BinaryQuadraticModel, sample_label: str) -> dict[int, int]:
-    """Ask the DWave sampler to return the best sample for the supplied binary quadratic model
-    
-    Args:
-        bqm (dimod.BinaryQuadraticModel):
-            Binary qudratic model representing the problem
-        sample_label (str):
-            Label to use for the sample - appears in the DWave dashboard
-
-        Returns:
-            A dictionary mapping the location of each element in the input "vector" to the
-            corresponding 0/1 value returned by the sampler
-    """
-    sampler = LeapHybridSampler()
-    sample_set = sampler.sample(bqm, label = sample_label)
-    return sample_set.first.sample
-
-
 def partition_numbers(set_of_numbers: list[int]) -> tuple[list[int], list[int]]:
     """Solves the number partitioning problem for the supplied set of numbers
 
@@ -62,7 +44,7 @@ def partition_numbers(set_of_numbers: list[int]) -> tuple[list[int], list[int]]:
     """
     bqm = generate_partition_problem_bqm(set_of_numbers)
     sample_label = "Partition Problem (" + str(len(set_of_numbers)) + " numbers)"
-    partitioning_info = sample_dwave(bqm, sample_label)
+    partitioning_info = sampler.sample_dwave(bqm, sample_label)
     assert (len(set_of_numbers) == len(partitioning_info)), "Something went wrong... the partitioning info doesn't match the set of input numbers."
     
     subset_of_numbers1: list[int] = list()
---
+++ sampler.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+import dimod
+from dwave.system import LeapHybridSampler
+
+
+def sample_dwave(bqm: dimod.BinaryQuadraticModel, sample_label: str) -> dict[int, int]:
+    """Ask the DWave sampler to return the best sample for the supplied binary quadratic model
+    
+    Args:
+        bqm (dimod.BinaryQuadraticModel):
+            Binary qudratic model representing the problem
+        sample_label (str):
+            Label to use for the sample - appears in the DWave dashboard
+
+        Returns:
+            A dictionary mapping the location of each element in the input "vector" to the
+            corresponding 0/1 value returned by the sampler
+    """
+    sampler = LeapHybridSampler()
+    sample_set = sampler.sample(bqm, label = sample_label)
+    return sample_set.first.sample
