9e1ec3c3ec5df37ba0b11dbedd1d75d58b3962a0
hvidberrrg <s.hvidberg@gmail.com>
2021-04-28 18:21:35 +0000

    First take on the maximum cut problem

---
+++ maximum_cut.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+from collections import defaultdict
+import dimod
+import networkx as nx
+import sampler
+
+
+def generate_maximum_cut_bqm(G: nx.Graph) -> dimod.BinaryQuadraticModel:
+    """Generates an instance of BinaryQuadraticModel with a QUBO formulation of the maximum cut
+    for the graph given as argument.
+
+    The implementation is based on Fred Glover et al: "Quantum Bridge Analytics I: A Tutorial on Formulating
+    and Using QUBO Models", arXiv:1811.11538
+
+    Args:
+        G (nx.Graph):
+            A graph
+
+    Returns:
+            An instance of BinaryQuadraticModel with a QUBO formulation of the maximum cut
+            problem for G.
+    """
+    linear: dict[int, int] = defaultdict(int)
+    quadratic: dict[tuple[int, int], int] = defaultdict(int)
+    for (i, j) in G.edges:
+        linear[i] += -1
+        linear[j] += -1
+        quadratic[(i, j)] = 2
+
+    offset = 0.0
+    vartype = dimod.BINARY
+    return dimod.BinaryQuadraticModel(linear, quadratic, offset, vartype)
+
+
+def maximum_cut(G: nx.Graph) -> tuple[set[int], set[int], set[tuple[int, int]], set[tuple[int, int]]]:
+    """Solves the maximum cut problem for the given graph, G. That is, the vertices
+    of G is separated into two complementary subsets, s and s_c, such that the number
+    of edges between s and s_c is as large as possible
+
+    Args:
+        G (nx.Graph):
+            A graph
+
+    Returns:
+            A tuple containing two complementary subsets of nodes, s and s_c, representing the
+            maximum cut for the graph G, along with two complementary sets of edges, comprising
+            the sets of cut and uncut edges respectively.
+    """
+    bqm = generate_maximum_cut_bqm(G)
+    sample_label = "Maximum cut (" + str(G.number_of_nodes()) + " nodes, " + str(G.number_of_edges()) + " edges)"
+    maximum_cut_info = sampler.sample_dwave(bqm, sample_label)
+    assert (G.number_of_nodes() == len(maximum_cut_info)), "Something went wrong... the maximum cut info doesn't match the input graph."
+    
+    nodes_set1: set[int] = set()
+    nodes_set2: set[int] = set()
+    for node in G.nodes:
+        if (maximum_cut_info[node]):
+            nodes_set1.add(node)
+        else:
+            nodes_set2.add(node)
+
+    cut_edges: set[tuple[int, int]] = set()
+    uncut_edges: set[tuple[int, int]] = set()
+    for (i, j) in G.edges:
+        if maximum_cut_info[i] != maximum_cut_info[j]:
+            cut_edges.add((i, j))
+        else:
+            uncut_edges.add((i, j))
+
+    return nodes_set1, nodes_set2, cut_edges, uncut_edges
---
+++ tests/test_maximum_cut.py
@@ -0,0 +1,46 @@
+import unittest
+import dimod
+import networkx as nx
+import maximum_cut as mc
+from mock import patch
+
+
+class MaximumCutTest(unittest.TestCase):
+
+    def setUp(self):
+        self.G = nx.Graph()
+        self.G.add_edges_from([(0, 1), (0, 3), (1, 2), (1, 3), (4, 2), (4, 3)])
+        self.maximum_cut_info = {0: 0, 1: 1, 2: 0, 3: 0, 4: 1}
+        
+    def test_generate_maximum_cut_bqm(self):
+        expected_linear_biases = {0: -2, 1: -3, 3: -3, 2: -2, 4: -2}
+        expected_quadratic_biases = {(0, 1): 2, (0, 3): 2, (1, 3): 2, (1, 2): 2, (3, 4): 2, (2, 4): 2}
+
+        bqm = mc.generate_maximum_cut_bqm(self.G)
+        self.assertEqual(bqm.linear, expected_linear_biases, "The linear biases of the BQM are not as expected")
+        self.assertEqual(bqm.quadratic, expected_quadratic_biases, "The quadratic biases of the BQM are not as expected")
+
+    def test_maximum_cut(self):    
+        # Patch LeapHybridSampler in the scope of the module it is imported into (i.e. sampler.py)
+        with patch('sampler.LeapHybridSampler') as mock_lhs:  
+            # Build a mock sample set - https://docs.ocean.dwavesys.com/en/stable/docs_dimod/reference/generated/dimod.SampleSet.from_samples.html    
+            mock_sampleset = dimod.SampleSet.from_samples([self.maximum_cut_info], dimod.BINARY, [1])
+            mock_lhs_instance = mock_lhs.return_value
+            mock_lhs_instance.sample.return_value = mock_sampleset
+
+            nodes_set1, nodes_set2, cut_edges, uncut_edges = mc.maximum_cut(self.G)
+            self.assertEqual(len(nodes_set1), 2, "Node set 1 is the wrong size")
+            self.assertTrue(1 in nodes_set1, "Node 1 should be in set0")
+            self.assertTrue(4 in nodes_set1, "Node 4 should be in set0")
+            self.assertEqual(len(nodes_set2), 3, "Node set 2 is the wrong size")
+            self.assertTrue(0 in nodes_set2, "Node 0 should be in set1")
+            self.assertTrue(2 in nodes_set2, "Node 2 should be in set1")
+            self.assertTrue(3 in nodes_set2, "Node 3 should be in set1")
+            self.assertEqual(len(cut_edges), 5, "The set of cut edges is the wrong size")
+            self.assertTrue((0, 1) in cut_edges, "Edge (0, 1) should be in the set of cut edges")
+            self.assertTrue((1, 2) in cut_edges, "Edge (1, 2) should be in the set of cut edges")
+            self.assertTrue((1, 3) in cut_edges, "Edge (1, 3) should be in the set of cut edges")
+            self.assertTrue((3, 4) in cut_edges, "Edge (3, 4) should be in the set of cut edges")
+            self.assertTrue((2, 4) in cut_edges, "Edge (2, 4) should be in the set of cut edges")
+            self.assertEqual(len(uncut_edges), 1, "The set of uncut edges is the wrong size")
+            self.assertTrue((0, 3) in uncut_edges, "Edge (0, 3) should be in the set of uncut edges")
